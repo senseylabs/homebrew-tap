f832e4dfe885fea1ef63e32fcf2bf6e96a9ddc25
btukus <berkhan.tukus@sensey.io>
2026-04-07 22:19:48 +0200

    Update kagi-cli to v0.2.2

--- Formula/kagi-cli.rb
+++ Formula/kagi-cli.rb
@@ -1,27 +1,27 @@
 class KagiCli < Formula
   desc "Kagi CLI — secrets management for Sensey"
   homepage "https://github.com/senseylabs/village"
-  version "0.2.1"
+  version "0.2.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://api.github.com/repos/senseylabs/village/releases/assets/391035682",
+      url "https://api.github.com/repos/senseylabs/village/releases/assets/391053355",
           headers: ["Accept: application/octet-stream", "Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}"]
-      sha256 "6e4e972833ee0c49a4e1165c2ded3a81c4a7ee9027e6cc815cb7b79568800fb5"
+      sha256 "a0dc95366652b71a5b21868ea3cf866211f8ca40c070607e30520994fc3b09c9"
     end
     on_intel do
-      url "https://api.github.com/repos/senseylabs/village/releases/assets/391035680",
+      url "https://api.github.com/repos/senseylabs/village/releases/assets/391053354",
           headers: ["Accept: application/octet-stream", "Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}"]
-      sha256 "34ed7b9813cf5750eaac39924158e56708976c561ef8b0b798ac0782606c076f"
+      sha256 "e18bbae37e89e2f600db335d9568493c27af604142176bf6ab1da5d98f4564c7"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://api.github.com/repos/senseylabs/village/releases/assets/391035685",
+      url "https://api.github.com/repos/senseylabs/village/releases/assets/391053357",
           headers: ["Accept: application/octet-stream", "Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}"]
-      sha256 "4446775a19b05a60b42cd09cd0f27c96a75431d834ad2ef8f82a09cc9c113fa5"
+      sha256 "77ca701986e70b20e89576dd4bae9af75eae9b4ed643fc5d16c3292b616e0e5a"
     end
   end
 
